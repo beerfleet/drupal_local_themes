2173b2d64b2116e739c3fbec16a264610e38dfbe
jvanbiervliet <jan.van.biervliet@etion.be>
2018-09-04 12:57:28 +0200

    initial commit

---
+++ mytheme/node.tpl.php
@@ -0,0 +1,32 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print $user_picture; ?>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <div class="content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+    
+  <?php if ($display_submitted): ?>
+    <h1 style="font-family: Arial; border: 1px #0000ff dotted;"><span class="submitted"><?php print $submitted ?></span></h1>
+  <?php endif; ?>
+    
+  <div class="clearfix">
+    <?php if (!empty($content['links'])): ?>
+      <div class="links"><?php print render($content['links']); ?></div>
+    <?php endif; ?>
+
+    <?php print render($content['comments']); ?>
+  </div>
+
+</div>
